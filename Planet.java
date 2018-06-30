137306340ace034c37789b667630ecc2946db215
je66yliu <je66y.liu@gmail.com>
2018-06-30 02:34:41 -0500

    day1

---
+++ Planet.java
@@ -0,0 +1,37 @@
+public class Planet {
+    public static final double G = 6.67e-11;
+    public double xxPos;
+    public double yyPos;
+    public double xxVel;
+    public double yyVel;
+    public double mass;
+    public String imgFileName;
+    public Planet(double xP, double yP, double xV, double yV, double m, String img){
+        xxPos = xP;
+        yyPos = yP;
+        xxVel = xV;
+        yyVel = yV;
+        mass = m;
+        imgFileName = img;
+    }
+    public Planet(Planet p){
+        xxPos = p.xxPos;
+        yyPos = p.yyPos;
+        xxVel = p.xxVel;
+        yyVel = p.yyVel;
+        mass = p.mass;
+        imgFileName = p.imgFileName;
+    }
+    public double calcDistance(Planet p){
+        return Math.sqrt(Math.pow(xxPos - p.xxPos, 2) + Math.pow(yyPos - p.yyPos, 2));
+    }
+    public double calcForceExertedBy(Planet p){
+        return (G*mass*p.mass)/Math.pow(calcDistance(p), 2);
+    }
+    public double calcForceExertedByX(Planet p){
+        return (calcForceExertedBy(p) * (p.xxPos - xxPos))/calcDistance(p);
+    }
+    public double calcForceExertedByY(Planet p){
+        return (calcForceExertedBy(p) * (p.yyPos - yyPos))/calcDistance(p);
+    }
+}
